6f9a5e295ef083046883e4016198d005479fb86b
Drag <drag.faq@gmail.com>
2016-11-20 00:04:30 +0200

    Level 5, lesson 7

--- src/com/javarush/test/level05/lesson07/task01/Friend.java
+++ src/com/javarush/test/level05/lesson07/task01/Friend.java
@@ -9,5 +9,25 @@ package com.javarush.test.level05.lesson07.task01;
 
 public class Friend
 {
+    private String name;
+    private int age;
+    private int sex;
+
     //напишите тут ваш код
+    public void initialize (String name)
+    {
+        this.name = name;
+    }
+
+    public void initialize (String name, int age)
+    {
+        this.age = age;
+        initialize(name);
+    }
+
+    public void initialize (String name, int age, int sex)
+    {
+        this.sex = sex;
+        initialize(name, age);
+    }
 }
