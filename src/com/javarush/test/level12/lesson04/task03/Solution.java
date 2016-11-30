da8ee235cdb6c944b089329f3da6ccda80456a7e
Drag <drag.faq@gmail.com>
2016-11-30 16:03:47 +0200

    Level 12 / lesson 4

---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    //Напишите тут ваши методы
+    public void print (int i)
+    {
+
+    }
+
+    public void print (String s)
+    {
+
+    }
+
+    static void print (Integer ii)
+    {
+
+    }
+
+    static void print (float f)
+    {
+
+    }
+
+    static void print (double d)
+    {
+
+    }
+
+}
