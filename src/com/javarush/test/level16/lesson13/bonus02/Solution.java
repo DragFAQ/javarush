058f513c2f7e08e15636c0cf3fec9e711bbc6826
Drag <drag.faq@gmail.com>
2017-01-06 00:24:02 +0200

    Resolved

--- src/com/javarush/test/level16/lesson13/bonus02/Solution.java
+++ src/com/javarush/test/level16/lesson13/bonus02/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level16.lesson13.bonus02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,23 +18,132 @@ import java.util.List;
 Подсказка: Нить 4 можно проверить методом isAlive()
 */
 
-public class Solution {
+public class Solution
+{
     public static List<Thread> threads = new ArrayList<Thread>(5);
 
-    static {
-        threads.add(new Thread(new Thread1()));
-        threads.add(new Thread(new Thread2()));
-        threads.add(new Thread(new Thread3()));
-        threads.add(new Thread(new Thread4()));
-        threads.add(new Thread(new Thread5()));
+    static
+    {
+        threads.add(new Thread1());
+        threads.add(new Thread2());
+        threads.add(new Thread3());
+        threads.add(new Thread4());
+        threads.add(new Thread5());
     }
 
-    public static class Thread1 implements Runnable
+    public static void main(String[] args){
+
+        Thread thread2 = threads.get(1);
+        thread2.start();
+        thread2.interrupt();
+
+        Thread thread4 = threads.get(3);
+        Message message = (Message) thread4;
+        thread4.start();
+        message.showWarning();
+        System.out.println(thread4.isAlive());
+
+    }
+    private static class Thread1 extends Thread
     {
         @Override
         public void run()
         {
+            while (true)
+            {
+            }
+        }
+    }
 
+    private static class Thread2 extends Thread
+    {
+        @Override
+        public void run()
+        {
+            try
+            {
+                while (!isInterrupted())
+                {
+                }
+                throw new InterruptedException();
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("InterruptedException");
+            }
+        }
+    }
+
+    private static class Thread3 extends Thread
+    {
+        @Override
+        public void run()
+        {
+            try
+            {
+                while (true)
+                {
+                    System.out.println("Ура");
+                    Thread.sleep(500);
+                }
+            }
+            catch (InterruptedException e)
+            {
+            }
+        }
+    }
+
+    private static class Thread4 extends Thread implements Message
+    {
+        @Override
+        public void showWarning()
+        {
+            interrupt();
+            try
+            {
+                join();
+            }
+            catch (InterruptedException e)
+            {
+
+            }
+        }
+
+        @Override
+        public void run()
+        {
+            while (!isInterrupted())
+            {
+            }
+        }
+    }
+
+    private static class Thread5 extends Thread
+    {
+        @Override
+        public void run()
+        {
+            int sum = 0;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            try
+            {
+                while (!isInterrupted())
+                {
+                    String line = reader.readLine();
+                    if (line.equals("N"))
+                        break;
+                    sum += Integer.parseInt(line);
+                }
+                throw new InterruptedException();
+            }
+            catch (IOException e)
+            {
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println(sum);
+            }
         }
     }
 }
