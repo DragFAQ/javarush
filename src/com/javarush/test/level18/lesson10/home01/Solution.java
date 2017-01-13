4ffe04d801d789e00a3ddbbcf0e9a34fd4807334
Drag <drag.faq@gmail.com>
2017-01-13 23:02:14 +0200

    Resolved

--- src/com/javarush/test/level18/lesson10/home01/Solution.java
+++ src/com/javarush/test/level18/lesson10/home01/Solution.java
@@ -7,7 +7,30 @@ package com.javarush.test.level18.lesson10.home01;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length > 0)
+        {
+            FileInputStream file = new FileInputStream(args[0]);
+            int count = 0;
+
+            while (file.available() > 0)
+            {
+                int i = file.read();
+                if (    (i >= Integer.valueOf('A') && i <= Integer.valueOf('Z')) ||
+                        (i >= Integer.valueOf('a') && i <= Integer.valueOf('z')))
+                    count ++;
+            }
+
+            System.out.println(count);
+
+            file.close();
+        }
     }
 }
