d21df63fd6eb32883fa7c8268bfcbd6d3acf6071
Drag <drag.faq@gmail.com>
2017-01-20 17:33:43 +0200

    Resolved

--- src/com/javarush/test/level19/lesson10/bonus02/FileConsoleWriter.java
+++ src/com/javarush/test/level19/lesson10/bonus02/FileConsoleWriter.java
@@ -6,6 +6,56 @@ package com.javarush.test.level19.lesson10.bonus02;
 При записи данных в файл, должен дублировать эти данные на консоль
 */
 
-public class FileConsoleWriter {
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FileConsoleWriter extends FileWriter
+{
+
+    public FileConsoleWriter(File file, boolean append) throws IOException
+    {
+        super(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd)
+    {
+        super(fd);
+    }
+
+    public FileConsoleWriter(File file) throws IOException
+    {
+
+        super(file);
+    }
+
+    public FileConsoleWriter(String fileName, boolean append) throws IOException
+    {
+
+        super(fileName, append);
+    }
+
+    public FileConsoleWriter(String fileName) throws IOException
+    {
+        super(fileName);
+    }
+
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        super.write(str, off, len);
+        System.out.println(str.substring(off, off + len));
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        super.write(cbuf, off, len);
+        System.out.println(String.valueOf(cbuf).substring(off, off + len));
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        super.write(c);
+        System.out.print((char)c);
+    }
 }
--- src/com/javarush/test/level19/lesson10/bonus03/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus03/Solution.java
@@ -26,7 +26,44 @@ text2>text1</tag>
 text1, text2 могут быть пустыми
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length > 0)
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+            String sCurrentLine, allFile = "";
+            while ((sCurrentLine = fileReader.readLine()) != null)
+                allFile += sCurrentLine + "\n";
+            Map<Integer, String> map = new TreeMap<>();
+
+            Pattern p = Pattern.compile("<\\/?" + args[0] + "[^>]*>");
+            Matcher m = p.matcher(allFile);
+            Stack<Integer> tags = new Stack<>();
+            while (m.find())
+            {
+                if (allFile.substring(m.start() + 1, m.start() + 2).equals("/"))
+                {
+                    int i = tags.pop();
+                    map.put(i, allFile.substring(i, m.end()));
+                }
+                else
+                    tags.push(m.start());
+            }
+
+            for (Map.Entry<Integer, String> pair : map.entrySet())
+                System.out.println(pair.getValue());
+        }
     }
 }
