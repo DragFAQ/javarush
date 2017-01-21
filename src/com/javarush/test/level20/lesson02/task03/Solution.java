6bc056838442363510c4eaba5838ff6137a87280
Drag <drag.faq@gmail.com>
2017-01-21 20:42:18 +0200

    Resolved

--- src/com/javarush/test/level20/lesson02/task03/Solution.java
+++ src/com/javarush/test/level20/lesson02/task03/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.test.level20.lesson02.task03;
 
 import java.io.*;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /* Знакомство с properties
 В методе fillInPropertiesMap считайте имя файла с консоли и заполните карту properties данными из файла.
@@ -8,17 +12,38 @@ import java.io.*;
 Реализуйте логику записи в файл и чтения из файла для карты properties.
 */
 public class Solution {
-//    public static Map<String, String> properties = new HashMap<>();
+    public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
+    public void fillInPropertiesMap() throws Exception
+    {
         //implement this method - реализуйте этот метод
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        InputStream inputStream = new FileInputStream(reader.readLine());
+        reader.close();
+
+        load(inputStream);
+        inputStream.close();
     }
 
     public void save(OutputStream outputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        for (Map.Entry<String, String> pair : properties.entrySet())
+            prop.put(pair.getKey(), pair.getValue());
+        prop.store(outputStream, "");
     }
 
     public void load(InputStream inputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        prop.load(inputStream);
+        properties.clear();
+        Enumeration<?> e = prop.propertyNames();
+        while (e.hasMoreElements())
+        {
+            String key = (String) e.nextElement();
+            String value = prop.getProperty(key);
+            properties.put(key, value);
+        }
     }
 }
