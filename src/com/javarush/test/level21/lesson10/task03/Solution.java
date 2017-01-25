e560d24ca037fa46253c38bb5cd9048fc76f9bf4
Drag <drag.faq@gmail.com>
2017-01-25 21:35:26 +0200

    Resolved

--- src/com/javarush/test/level21/lesson10/task03/Solution.java
+++ src/com/javarush/test/level21/lesson10/task03/Solution.java
@@ -72,7 +72,13 @@ public class Solution {
     public static class SpecificUtilizator extends Utilizator {
         @Override
         public void dispose() {
-            util.throwException();
+            try
+            {
+                util.throwException();
+            }
+            catch (Exception e)
+            {
+            }
         }
     }
 
