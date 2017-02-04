2a3a968ce7ef5940405c9efaebe9bb75d5d18037
Drag <drag.faq@gmail.com>
2017-02-04 21:10:51 +0200

    Resolved

--- src/com/javarush/test/level23/lesson04/home01/Solution.java
+++ src/com/javarush/test/level23/lesson04/home01/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level23.lesson04.home01;
 
+import java.util.List;
+import java.util.Map;
+
 /* Inner 3
 Внутри класса Solution:
 1) реализуйте private class TaskDataProvider используя Task и DbMock, цель которого - обновить поле tasks.
@@ -28,5 +31,25 @@ public class Solution {
 
 
     class Task {
+    }
+
+    private class TaskDataProvider implements DbDataProvider
+    {
+
+        @Override
+        public void refreshAllData(Map criteria)
+        {
+            tasks = DbMock.getFakeTasks(criteria);
+        }
+    }
+
+    private class NameDataProvider implements DbDataProvider
+    {
+
+        @Override
+        public void refreshAllData(Map criteria)
+        {
+            names = DbMock.getFakeNames(criteria);
+        }
     }
 }
--- src/com/javarush/test/level23/lesson04/task01/Solution.java
+++ src/com/javarush/test/level23/lesson04/task01/Solution.java
@@ -8,11 +8,19 @@ package com.javarush.test.level23.lesson04.task01;
 public class Solution {
     public InnerClass[] innerClasses = new InnerClass[2];
 
-    public class InnerClass {
+    public static class InnerClass {
     }
 
     public static Solution[] getTwoSolutions() {
+        Solution[] result = new Solution[2];
 
-        return null;
+        result[0] = new Solution();
+        result[0].innerClasses[0] = new InnerClass();
+        result[0].innerClasses[1] = new InnerClass();
+        result[1] = new Solution();
+        result[1].innerClasses[0] = new InnerClass();
+        result[1].innerClasses[1] = new InnerClass();
+
+        return result;
     }
 }
