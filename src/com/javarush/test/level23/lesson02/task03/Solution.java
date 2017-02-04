32f0aee6b1dfa13db2b48f54037a4f3d69b2cee6
Drag <drag.faq@gmail.com>
2017-02-04 20:41:56 +0200

    Resolved

--- src/com/javarush/test/level23/lesson02/task03/Solution.java
+++ src/com/javarush/test/level23/lesson02/task03/Solution.java
@@ -5,13 +5,17 @@ package com.javarush.test.level23.lesson02.task03;
 */
 public class Solution {
 
-    public static class Listener {
+    public abstract static class Listener {
         public void onMouseDown(int x, int y) {
             //do something on mouse down event
         }
 
         public void onMouseUp(int x, int y) {
             //do something on mouse up event
+        }
+
+        private Listener()
+        {
         }
     }
 }
