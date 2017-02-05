c7e4a87f1381aa4b0385361ffe9b0fc626d10fad
Drag <drag.faq@gmail.com>
2017-02-05 02:12:46 +0200

    Resolved

--- src/com/javarush/test/level23/lesson06/task02/Solution.java
+++ src/com/javarush/test/level23/lesson06/task02/Solution.java
@@ -4,45 +4,66 @@ package com.javarush.test.level23.lesson06.task02;
 Отрефакторите класс Solution: вынесите все константы в public вложенный(nested) класс Constants.
 Запретите наследоваться от Constants.
 */
-public class Solution {
+public class Solution
+{
 
-    public class ServerNotAccessibleException extends Exception {
-        public ServerNotAccessibleException() {
-            super("Server is not accessible for now.");
+    public class ServerNotAccessibleException extends Exception
+    {
+        public ServerNotAccessibleException()
+        {
+            super(Constants.NO_ACCESS);
         }
 
-        public ServerNotAccessibleException(Throwable cause) {
-            super("Server is not accessible for now.", cause);
-        }
-    }
-
-    public class UnauthorizedUserException extends Exception {
-        public UnauthorizedUserException() {
-            super("User is not authorized.");
-        }
-
-        public UnauthorizedUserException(Throwable cause) {
-            super("User is not authorized.", cause);
+        public ServerNotAccessibleException(Throwable cause)
+        {
+            super(Constants.NO_ACCESS, cause);
         }
     }
 
-    public class BannedUserException extends Exception {
-        public BannedUserException() {
-            super("User is banned.");
+    public class UnauthorizedUserException extends Exception
+    {
+        public UnauthorizedUserException()
+        {
+            super(Constants.NO_AUTHORIZE);
         }
 
-        public BannedUserException(Throwable cause) {
-            super("User is banned.", cause);
+        public UnauthorizedUserException(Throwable cause)
+        {
+            super(Constants.NO_AUTHORIZE, cause);
         }
     }
 
-    public class RestrictionException extends Exception {
-        public RestrictionException() {
-            super("Access is denied.");
+    public class BannedUserException extends Exception
+    {
+        public BannedUserException()
+        {
+            super(Constants.BANNED);
         }
 
-        public RestrictionException(Throwable cause) {
-            super("Access is denied.", cause);
+        public BannedUserException(Throwable cause)
+        {
+            super(Constants.BANNED, cause);
         }
+    }
+
+    public class RestrictionException extends Exception
+    {
+        public RestrictionException()
+        {
+            super(Constants.ACCESS_DENIED);
+        }
+
+        public RestrictionException(Throwable cause)
+        {
+            super(Constants.ACCESS_DENIED, cause);
+        }
+    }
+
+    public static final class Constants
+    {
+        public final static String NO_ACCESS = "Server is not accessible for now.";
+        public final static String NO_AUTHORIZE = "User is not authorized.";
+        public final static String BANNED = "User is banned.";
+        public final static String ACCESS_DENIED = "Access is denied.";
     }
 }
